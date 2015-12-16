afb5acc063f3b33d76d0b0ca5b3faef55f35fe80
osvifl1516daw2 <osvifl1516daw2@gmail.com>
2015-12-16 15:26:47 +0100

    Treballant amb Subversion i Github

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -19,7 +19,7 @@ public class Jenkins {
             bw.write("<html>");bw.newLine();
             bw.write("  <head>");bw.newLine();
             bw.write("    <title>");bw.newLine();
-            bw.write("      Treballant amb l'IC Jenkins");bw.newLine();
+            bw.write("      Treballant amb subversion");bw.newLine();
             bw.write("    </title>");bw.newLine();
             bw.write("  </head>");bw.newLine();
             bw.write("  <body>");bw.newLine();
